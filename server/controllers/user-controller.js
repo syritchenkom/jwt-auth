7c7263eb68561be6f2e6192d531c2fc0fc8b61e3
syritchenkom <syritchenko@gmail.com>
2023-07-13 23:41:22 +0200

    Corrected the code

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -6,12 +6,7 @@ class UserController {
             const {email, password} = req.body;
             const  userData = await userService.registration(email, password);
             res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true});
-            return res.json(userData);
-
-            
-            //Video on 36:33 Generate JWT
-        
-        
+            return res.json(userData);      
         }catch(err){
             console.log(err);   
         }
