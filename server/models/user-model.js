ddc746478abaacc6d133cde07a24ecb4e6d0874e
syritchenkom <syritchenko@gmail.com>
2023-07-13 23:46:10 +0200

    Corrected the code

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -1,10 +1,10 @@
 const {Schema, model} = require('mongoose');
 
 const UserSchema = new Schema({
-    email: {type: String, unique: true, require: true},
-    password: {type: String, require: true},
+    email: {type: String, unique: true, required: true},
+    password: {type: String, required: true},
     isActivated: {type: Boolean, default: false},
     activationLink: {type: String},
 });
 
-model.export = model('User', UserSchema);
+module.exports = model('User', UserSchema);
