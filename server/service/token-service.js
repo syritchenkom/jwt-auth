709e0db40ab68189185072682c5be4b7effeccbd
syritchenkom <syritchenko@gmail.com>
2023-07-14 23:34:22 +0200

    Correction errors

--- server/service/token-service.js
+++ server/service/token-service.js
@@ -1,8 +1,8 @@
-const jwt = require('jwt');
+const jwt = require('jsonwebtoken');
 const tokenModel = require('../models/token-model');
 
 class TokenService {
-    generateToken(payload){
+    generateTokens(payload){
         const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, {expiresIn:'30m'});
         const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, {expiresIn:'30d'});
         return {
@@ -15,7 +15,7 @@ class TokenService {
         const tokenData = await tokenModel.findOne({user: userId})
         if(tokenData){
             tokenData.refreshToken = refreshToken;
-            return tokenData,save();
+            return tokenData.save();
         }
         const token = await tokenModel.create({user: userId, refreshToken})
         return token;
