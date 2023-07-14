b729f7256d3782881e2df268d8453ae761c8d2a7
syritchenkom <syritchenko@gmail.com>
2023-07-14 23:39:10 +0200

    Correction code

--- server/service/user-service.js
+++ server/service/user-service.js
@@ -1,4 +1,4 @@
-const UserModel = require('../models/UserModel');
+const UserModel = require('../models/user-model');
 const bcrypt = require('bcrypt');
 const uuid = require('uuid');
 const mailService = require('./mail-service');
@@ -8,24 +8,27 @@ const UserDto = require('../dtos/user-dto');
 class UserService {
     async registration(email, password) {
         //Checking the registered e-mail address in the database or not?
-        const candidates = await UserModel.findOne({email})
+        const candidate = await UserModel.findOne({email})
         //If registered get error
-        if(candidates){
+        if(candidate){
             throw new Error(`User with email ${email} is in database already`)
         }
         //In this hashing password
-        const hashPassword = await bcrypt.hash(password, 3)
+        const hashPassword = await bcrypt.hash(password, 3); 
         //Here we have link to activation
         const activationLink = uuid.v4(); //v34fa-asfasf- 142saf-sa-asf
+        
         //Save user to database
         const user = await UserModel.create({email, password: hashPassword, activationLink})
         //Send a latter to the e-mail address
-        await mailService.sendActivationMail(email, activationLink);
+        await mailService.sendActivationMail(email, `${process.env.API_URL}/api/activate/${activationLink}`);//?????
+       
         //Generate token
         const userDto = new UserDto(user); // id , email, isActivated
         //Save token to database
-        const tokens = tokenService.generateToken({...UserDto});
+        const tokens = tokenService.generateTokens({...userDto});
         await tokenService.saveToken(userDto.id, tokens.refreshToken);
+        
         //Return info about tokens and user
         return {...tokens, user: userDto}
     }
