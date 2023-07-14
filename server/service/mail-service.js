5f738ed1b35e204a6377e4dd961e555ab1b9d3c4
syritchenkom <syritchenko@gmail.com>
2023-07-14 23:27:35 +0200

    Sending confirmation by e-mail

--- server/service/mail-service.js
+++ server/service/mail-service.js
@@ -1,5 +1,33 @@
+const nodemailer = require('nodemailer');
+
 class MailService {
-    async sendActivationMail(to, link){}
+
+    constructor() {
+        this.transporter = nodemailer.createTransport({
+            host: process.env.SMTP_HOST,
+            port: process.env.SMTP_PORT,
+            secure: false,
+            auth: {
+                user: process.env.SMTP_USER,
+                password: process.env.SMTP_PASSWORD,
+            }
+        })
+    }
+    async sendActivationMail(to, link){
+        await this.transporter.sendMail({
+            from: process.env.SMTP_USER,
+            to,
+            subject: 'Account activation on ' + process.env.API_URL,
+            test: '',
+            html: 
+                `
+                    <div>
+                        <h1>For activation click the link below</h1>
+                        <a href="${link}">${link}</a>
+                    </div>
+                `
+        })
+    }
 }
 
 module.exports = new MailService();
